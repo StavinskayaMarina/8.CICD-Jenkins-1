8d65986451c15f72899d2fef1a795e93ddb0ca18
StavinskayaMarina <128706641+StavinskayaMarina@users.noreply.github.com>
2024-01-12 18:19:28 +0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "4b479h",
+  projectId: "m6x7ip",
   e2e: {
     baseUrl: "http://qamid.tmweb.ru",
     setupNodeEvents(on, config) {},
